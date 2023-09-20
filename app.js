c10e0ec42818a657049fd0e69253c889b75ae681
sethh99 <145517441+sethh99@users.noreply.github.com>
2023-09-20 09:19:15 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+var change = document.querySelector('#box-img');
+document.querySelector('#img1').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_1.jpg');
+//  alert('test')
+})
+document.querySelector('#img1').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img2').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_2.jpg');
+//  alert('test')
+})
+document.querySelector('#img2').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+document.querySelector('#img3').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_three.jpg');
+//  alert('test')
+})
+document.querySelector('#img3').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img4').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_10.jpg');
+//  alert('test')
+})
+document.querySelector('#img4').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img5').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+document.querySelector('#img5').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img6').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_6.jpg');
+//  alert('test')
+})
+document.querySelector('#img6').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img7').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_7.jpg');
+//  alert('test')
+})
+document.querySelector('#img7').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img8').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_8.jpg');
+//  alert('test')
+})
+document.querySelector('#img8').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+document.querySelector('#img9').addEventListener("mouseover" , function(){
+    change.setAttribute('src' , 'photo/dis_9.jpg');
+//  alert('test')
+})
+document.querySelector('#img9').addEventListener("mouseout" , function(){
+    change.setAttribute('src' , 'photo/dis_5.jpg');
+//  alert('test')
+})
+
+
+
+
+
+
